8d2281ec5aa61c2f654d9e22b896278a53e8f949
naoyanaonaoya <naoya.basket.a.i@gmail.com>
2022-06-27 11:53:10 +0900

    links

--- tutorial/src/pages/company/history.js
+++ tutorial/src/pages/company/history.js
@@ -1,7 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const history = () => {
-  return <div>this sis history of our company</div>
+  return (
+    <>
+      <div>
+        <h2>this sis history of our company</h2>
+        <Link to="/">home</Link>
+      </div>
+    </>
+  )
 }
 
 export default history
--- tutorial/src/pages/index.js
+++ tutorial/src/pages/index.js
@@ -1,9 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
   return (
     <>
       <div>Hello world!</div>
+      <div>
+        <Link to="/about">about</Link>
+      </div>
+      <div>
+        <Link to="/company">company</Link>
+      </div>
+      <div>
+        <Link to="/company/history">history</Link>
+      </div>
     </>
   )
 }
