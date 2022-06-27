59b52f31a0810696d20a3d2b1c0ade9c9bcb6865
naoyanaonaoya <naoya.basket.a.i@gmail.com>
2022-06-27 13:14:00 +0900

    style

--- tutorial/src/pages/about.js
+++ tutorial/src/pages/about.js
@@ -1,14 +1,30 @@
 import React from "react"
 import Layout from "../components/Layout"
+import styled from "styled-components"
 
 const about = () => {
   return (
     <>
       <Layout>
+        <Wrapper>
+          <h1>about page</h1>
+          <h1>hello world</h1>
+          <p className="text">asdgf aljkdl;tja agjasty adfd</p>
+        </Wrapper>
         <h2>about page</h2>
       </Layout>
     </>
   )
 }
+
+const Wrapper = styled.section`
+  color: red;
+  h1 {
+    color: yellowgreen;
+  }
+  .text {
+    text-transform: uppercase;
+  }
+`
 
 export default about
--- tutorial/src/pages/index.js
+++ tutorial/src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { ExampleButton } from "../examples/button"
+import styled from "styled-components"
 
 export default function Home() {
   return (
     <>
       <Layout>
+        <ExampleButton>click me</ExampleButton>
         <h2>home page</h2>
       </Layout>
     </>
