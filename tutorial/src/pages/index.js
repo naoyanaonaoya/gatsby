950f765c08d64f5262c3b048b3297819176bc9a7
naoyanaonaoya <naoya.basket.a.i@gmail.com>
2022-06-27 21:29:49 +0900

    about page

--- tutorial/src/pages/index.js
+++ tutorial/src/pages/index.js
@@ -1,13 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
-import Images from "../examples/Images"
 
 export default function Home() {
   return (
     <>
       <Layout>
         <h2>home page</h2>
-        <Images />
       </Layout>
     </>
   )
